a8e5f4934119d8c887fd020735a4ccb583d393a1
Charles <mail@ctudor.net>
2015-11-16 18:58:45 +0000

    Added app icon and notification icon.

--- app/src/main/java/net/ctudor/housealert/MyGCMListenerService.java
+++ app/src/main/java/net/ctudor/housealert/MyGCMListenerService.java
@@ -67,7 +67,7 @@ public class MyGCMListenerService extends GcmListenerService {
 
         Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.drawable.ic_stat_ic_notification)
+                .setSmallIcon(R.drawable.housetrans)
                 .setContentTitle("HouseAlert")
                 .setContentText(message)
                 .setAutoCancel(true)
